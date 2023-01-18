3f6643bfa3646551917e895dab1431910785eca2
Harvey <harveytapang@gmail.com>
2023-01-18 18:20:01 +0800

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<script>
+export default{ 
+  data() {
+    return {
+      count : 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button @click="count++">Click Like and Subscribe! {{count}} 👍</button>
+  </div>
+</template>
