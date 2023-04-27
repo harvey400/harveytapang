8ab560c0a1e4035d4779717c0427c610c5cfff0b
Harvey <harveytapang@gmail.com>
2023-04-27 09:54:36 +0800

    Fix About Page display

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,27 @@
 <template>
-    <h1>About</h1>
+    <div>
+       <h1 class="green pointer unselectable" @click="setName"> {{ fullName }}</h1>
+    </div>
 </template>
+<script setup>
+import {ref, computed} from 'vue';
+
+const firstName = ref("Harvey");
+const lastName = ref("Tapang");
+const nameChange = ref(1);
+
+const fullName = computed(() => firstName.value + " " + lastName.value);
+
+function setName(){
+
+    if(nameChange.value === 1){
+        firstName.value = "Charlene"
+        lastName.value = "Cruz"
+    } else {
+        firstName.value = "Harvey"
+        lastName.value = "Tapang"
+    }
+    
+    nameChange.value *= -1;
+}
+</script>
