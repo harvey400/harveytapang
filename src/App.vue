0064be0202f9e8e2432b6bc9d7f1eb47da96964b
Harvey <harveytapang@gmail.com>
2023-04-27 11:05:45 +0800

    Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <ul>
+    <ul class="nav">
       <li><router-link :to="{name:'about'}">About</router-link></li>
       <li><router-link :to="{name: 'projects'}">Projects</router-link></li>
     </ul>
@@ -14,5 +14,33 @@
   import { RouterView, RouterLink } from 'vue-router';
 </script>
 <style scoped>
+  ul {
+    list-style-type:none;
+    margin:0;
+    padding:0;
+    overflow:hidden;
+    background-color: #333;
+
+  }
+
+  li {
+    float: left;
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  li a:hover:not(.router-link-active) {
+    background-color: #111;
+  }
+
+  .router-link-active {
+    background-color:hsla(160, 100%, 37%, 1);;
+  }
 
 </style>
