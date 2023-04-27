54b4a1147a7c37bacef3c53c363eea7728cb8a58
Harvey <harveytapang@gmail.com>
2023-04-27 09:13:53 +0800

    Remove unneccessary css style, Add info

---
+++ src/components/Information.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref, computed} from "vue"
+const firstName = ref("Charlene")
+const lastName = ref("Cruz");
+
+const fullName = computed(()=> firstName.value + " " + lastName.value);
+
+function test() {
+    firstName.value = "Harvey"
+    lastName.value = "Tapang"
+}
+
+</script>
+<template>
+    <div>
+        Developer <h1 class="green">{{ fullName }}</h1>
+        <button @click="test">Change Name</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
