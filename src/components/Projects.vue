f54fe2271a756a2a0b7b57cc681b70886d3054c6
Harvey <harveytapang@gmail.com>
2024-07-12 10:04:02 +0800

    Added image transition for hovering projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,7 +2,7 @@
     <div class="h-60"></div>
     <div class="text-white w-full h-fit ml-20">
         <div class="text-3xl sm:text-5xl md:text-5xl font-semibold mb-5">Projects</div>
-        <a :href="project.link" target="_blank" v-for="(project, key) in projects" class="pointer" @mouseenter="toggleProjectBg(key,1)" @mouseleave="toggleProjectBg(key,0)" >
+        <a :href="project.link" target="_blank" v-for="(project, projectKey) in projects" class="pointer" @mouseenter="toggleProjectBg(projectKey,1)" @mouseleave="toggleProjectBg(projectKey,0)" >
             <div class="grid md:grid-cols-2 sm:grid-cols-1 sm:my-[100px] md:my-[300px]">
                 <div class="">
                     <div class="text-vueGreen text-2xl font-semibold mb-3">{{ project.title }}</div>
@@ -10,10 +10,12 @@
                 </div>
                 <div class="flex justify-center">
                     <div class="relative sm:mt-10 lg:mt-0">
-                        <div :id="'bg-project-' + key" class="bg-project flex justify-center lg:visible md:visible sm:invisible">
+                        <div :id="'bg-project-' + projectKey" class="bg-project flex justify-center lg:visible md:visible sm:invisible">
                             <img :src="'img/project-preview/' + project['bg-preview-image']">
                         </div>
-                        <img class="project-thumbnail border" :src="'img/project-preview/' + project['preview-image']" width="300px">
+                        <div class="project-thumbnail-container m-5">
+                            <img class="project-thumbnail border project-thumbnail-hide" :class="'project-thumbnail-' + projectKey" v-for="previewImage in project['preview-images']" :src="'img/project-preview/' + previewImage">
+                        </div>
                     </div>
                 </div>
             </div>
@@ -27,17 +29,66 @@ import { ref, onMounted} from 'vue';
 
 const projects = ref(Projects);
 
+let stopTransition;
+
 const toggleProjectBg = (key,value) => {
     let bgProject = document.querySelector('#bg-project-' + key).classList;
     if (value) {
         bgProject.add('clip-circle');
+        previewImages(key)
+
     } else {
         bgProject.remove('clip-circle');
+        clearInterval(stopTransition);
+        resetPreviewImages(key)
     }
 }
 
-onMounted(()=> {
+//Start animation on preview image
+let previewImages = (projectKey) => {
+    let pt = document.querySelectorAll('.project-thumbnail-' + projectKey);
+    let currentKey = pt.length == 1 ? 0 : 1;
     
+    stopTransition = setInterval(() => {
+        pt.forEach((element,key) => {
+            if(key == currentKey){
+                pt[key].classList.add('project-thumbnail-show')
+                pt[key].classList.remove('project-thumbnail-hide')
+            } else {
+                pt[key].classList.add('project-thumbnail-hide')
+                pt[key].classList.remove('project-thumbnail-show')
+            }
+        });
+        currentKey++;
+        if(currentKey >= pt.length)  currentKey = 0;
+    }, 1500);
+}
+
+//reset animation on preview image
+let resetPreviewImages = (key) => {
+    let pt = document.querySelectorAll('.project-thumbnail-' + key);
+    let currentKey = 0;
+    pt.forEach((element,key) => {
+            if(key == currentKey){
+                pt[key].classList.add('project-thumbnail-show')
+                pt[key].classList.remove('project-thumbnail-hide')
+            } else {
+                pt[key].classList.add('project-thumbnail-hide')
+                pt[key].classList.remove('project-thumbnail-show')
+            }
+        });
+}
+
+let initialize = () =>{
+    Projects.forEach((element,key) => {
+        let pt = document.querySelectorAll('.project-thumbnail-' + key);
+        pt[0].classList.add('project-thumbnail-show')
+        pt[0].classList.remove('project-thumbnail-hide')
+    })
+}
+
+onMounted(()=> {
+    initialize();
 });
 
 </script>
@@ -61,8 +112,23 @@ onMounted(()=> {
 }
 
 .project-thumbnail {
-    height: auto;
+    height: 240px;
     width: 400px;
+    position: absolute;
 }
 
+.project-thumbnail-container{
+    width: 400px;
+    height: 235px;
+}
+
+.project-thumbnail-hide {
+    transition: 1s ease-in;
+    opacity: 0;
+}
+
+.project-thumbnail-show {
+    transition: 1s ease-in;
+    opacity: 1;
+}
 </style>
